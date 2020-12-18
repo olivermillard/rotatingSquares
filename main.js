2ac51f5e448af7f93cabfc5109f97504cb21e6c9
Oliver Millard <olivermillard@gmail.com>
2020-12-17 20:26:05 -0600

    changes

--- main.js
+++ main.js
@@ -19,55 +19,138 @@ function getHeight() {
     document.documentElement.clientHeight
   );
 }
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+var colors = [
+  "blue",
+  "red",
+  "green",
+  "purple",
+  "yellow",
+  "white",
+  "aquamarine",
+  "chartreuse",
+  "crimson",
+  "DarkOrange",
+  "HotPink",
+];
+
+var numCubesRow;
+var numCubesCol;
+var numCubes;
+
 function setup() {
   var pageWidth = getWidth();
   var pageHeight = getHeight();
 
-  var squareContainer = document.createElement("div");
-  squareContainer.setAttribute("id", "squareContainer");
+  var cubeContainer = document.createElement("div");
+  cubeContainer.setAttribute("id", "cubeContainer");
 
-  var marginWidth = (pageWidth % 20) / 2 + 20;
-  var containerWidth = pageWidth - (pageWidth % 20) - 40;
-  squareContainer.style.marginLeft = marginWidth + "px";
-  squareContainer.style.marginRight = marginWidth + "px";
-  squareContainer.style.width = containerWidth + "px";
+  var containerWidth = pageWidth - (pageWidth % 50) - 50;
+  cubeContainer.style.width = containerWidth + "px";
+  var containerHeight = pageHeight - (pageHeight % 50) - 100;
+  cubeContainer.style.height = containerHeight + "px";
 
-  var marginHeight = (pageHeight % 20) / 2 + 20;
-  var containerHeight = pageHeight - (pageHeight % 20) - 40;
-  squareContainer.style.marginTop = marginHeight + "px";
-  squareContainer.style.marginBottom = marginHeight + "px";
-  squareContainer.style.height = containerHeight + "px";
-
-  var prevsquareContainer = document.getElementById("squareContainer");
-  if (prevsquareContainer) {
-    mainContainer.removeChild(prevsquareContainer);
+  var prevcubeContainer = document.getElementById("cubeContainer");
+  if (prevcubeContainer) {
+    mainContainer.removeChild(prevcubeContainer);
   }
-  numCubesRow = containerWidth / 20;
-  numCubesCol = containerHeight / 20;
+  numCubesRow = containerWidth / 50;
+  numCubesCol = containerHeight / 50;
   numCubes = numCubesRow * numCubesCol;
 
-  var currRow = 0;
   for (var i = 0; i < numCubes; i++) {
-    var x = i % numCubesRow;
-    var y;
-    if (i % numCubesRow == numCubesRow - 1) {
-      y = currRow;
-      currRow += 1;
-    } else {
-      y = currRow;
-    }
     var newCube = document.createElement("div");
     newCube.className = "cube";
-    newCube.setAttribute("id", x + "-" + y);
-    newCube.onmouseover = function() {
-      spin(this.id);
-    };
-    squareContainer.appendChild(newCube);
+    var cubeID = "c" + i;
+    newCube.setAttribute("id", cubeID);
+    newCube.style.backgroundColor = colors[getRandomInt(colors.length)];
+    newCube.style.border = "5px solid " + colors[getRandomInt(colors.length)];
+    var content = document.createElement("div");
+    content.style.backgroundColor = colors[getRandomInt(colors.length)];
+    content.style.border = "5px solid " + colors[getRandomInt(colors.length)];
+    if (getRandomInt(2) == 0) {
+      content.className = "circle";
+    } else {
+      content.className = "square";
+    }
+    newCube.appendChild(content);
+    cubeContainer.appendChild(newCube);
   }
-  squareContainer.style.border = "1px solid darkblue";
-  mainContainer.appendChild(squareContainer);
+  cubeContainer.style.border = "5px solid white";
+  mainContainer.appendChild(cubeContainer);
+  setAnimations();
 }
 
-function spin(cubeID) {
-  var currCube = document.getElementById(cubeID);
+function setAnimations() {
+  //   const sleep = (milliseconds) => {
+  //     return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  //   };
+  //   const setNewAnimations = async () => {
+  for (var i = 0; i < numCubes; i++) {
+    var thisCube = document.getElementById("c" + i);
+    var animationNum = getRandomInt(4);
+    var animationType = "";
+    var animateFrom = "";
+    var animateTo = "";
+    var animationDuration = 2500;
+    //var animationDuration = 2 * (getRandomInt(2) + 1) * 1000;
+
+    if (animationNum == 0) {
+      animationType = "rotateX";
+      animateFrom = "0deg";
+      animateTo = "180deg";
+    } else if (animationNum == 1) {
+      animationType = "rotateX";
+      animateFrom = "180deg";
+      animateTo = "0deg";
+    } else if (animationNum == 2) {
+      animationType = "rotateY";
+      animateFrom = "0deg";
+      animateTo = "180deg";
+    } else {
+      animationType = "rotateY";
+      animateFrom = "180deg";
+      animateTo = "0deg";
+    }
+    thisCube.animate(
+      [
+        // keyframes
+        { transform: "" + animationType + "(" + animateFrom + ")" },
+        { transform: "" + animationType + "(" + animateTo + ")" },
+      ],
+      {
+        // timing options
+        duration: animationDuration,
+        iterations: 1,
+        timing: "ease",
+      }
+    );
+  }
+  var testCube = document.getElementById("c1");
+  Promise.all(
+    testCube
+      .getAnimations({ subtree: true })
+      .map((animation) => animation.finished)
+  ).then(() => setAnimations());
 }
+
+// thisCube.onmouseover = function() {
+//     var mouseoverCube = document.getElementById(this.id);
+//     mouseoverCube.animate(
+//       [
+//         // keyframes
+//         { transform: "rotateZ(" + animateFrom + ")" },
+//         { transform: "rotateZ(" + animateTo + ")" },
+//       ],
+//       {
+//         // timing options
+//         duration: animationDuration,
+//         iterations: Infinity,
+//         timing: "linear",
+//       }
+//     );
+//   };
