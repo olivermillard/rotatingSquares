9e1625dc14e6affa1bfdfda4eb6edc83cd908bb7
Oliver Millard <olivermillard@gmail.com>
2020-12-18 13:10:22 -0600

    speed changer

--- main.js
+++ main.js
@@ -70,6 +70,7 @@ function setup() {
     newCube.style.backgroundColor = colors[getRandomInt(colors.length)];
     newCube.style.border = "5px solid " + colors[getRandomInt(colors.length)];
     var content = document.createElement("div");
+    content.setAttribute("id", "i" + i);
     content.style.backgroundColor = colors[getRandomInt(colors.length)];
     content.style.border = "5px solid " + colors[getRandomInt(colors.length)];
     if (getRandomInt(2) == 0) {
@@ -85,52 +86,79 @@ function setup() {
   setAnimations();
 }
 
-function setAnimations() {
-  for (var i = 0; i < numCubes; i++) {
-    var thisCube = document.getElementById("c" + i);
-    var animationNum = getRandomInt(4);
-    var animationType = "";
-    var animateFrom = "";
-    var animateTo = "";
-    var animationDuration = 2500;
-    if (animationNum == 0) {
-      animationType = "rotateX";
-      animateFrom = "0deg";
-      animateTo = "180deg";
-    } else if (animationNum == 1) {
-      animationType = "rotateX";
-      animateFrom = "180deg";
-      animateTo = "0deg";
-    } else if (animationNum == 2) {
-      animationType = "rotateY";
-      animateFrom = "0deg";
-      animateTo = "180deg";
-    } else {
-      animationType = "rotateY";
-      animateFrom = "180deg";
-      animateTo = "0deg";
-    }
-    thisCube.animate(
-      [
-        // keyframes
-        { transform: "" + animationType + "(" + animateFrom + ")" },
-        { transform: "" + animationType + "(" + animateTo + ")" },
-      ],
-      {
-        // timing options
-        duration: animationDuration,
-        iterations: 1,
-        timing: "ease",
-      }
-    );
-  }
-  var testCube = document.getElementById("c1");
-  Promise.all(
-    testCube
-      .getAnimations({ subtree: true })
-      .map((animation) => animation.finished)
-  ).then(() => setAnimations());
+var animationDuration = 2600;
+function updateSpeed(speed) {
+  animationDuration = 5100 - speed;
 }
+
+function changeColors(i) {
+  var cube = document.getElementById("c" + i);
+  cube.style.backgroundColor = colors[getRandomInt(colors.length)];
+  cube.style.border = "5px solid " + colors[getRandomInt(colors.length)];
+  var contents = document.getElementById("i" + i);
+  contents.style.backgroundColor = colors[getRandomInt(colors.length)];
+  contents.style.border = "5px solid " + colors[getRandomInt(colors.length)];
+}
+
+function setAnimations() {
+  if (paused == false) {
+    for (var i = 0; i < numCubes; i++) {
+      var thisCube = document.getElementById("c" + i);
+      var animationNum = getRandomInt(4);
+      var animationType = "";
+      var animateFrom = "";
+      var animateTo = "";
+      if (animationNum == 0) {
+        animationType = "rotateX";
+        animateFrom = "0deg";
+        animateTo = "180deg";
+      } else if (animationNum == 1) {
+        animationType = "rotateX";
+        animateFrom = "180deg";
+        animateTo = "0deg";
+      } else if (animationNum == 2) {
+        animationType = "rotateY";
+        animateFrom = "0deg";
+        animateTo = "180deg";
+      } else {
+        animationType = "rotateY";
+        animateFrom = "180deg";
+        animateTo = "0deg";
+      }
+      thisCube.animate(
+        [
+          // keyframes
+          { transform: "" + animationType + "(" + animateFrom + ")" },
+          { transform: "" + animationType + "(" + animateTo + ")" },
+        ],
+        {
+          // timing options
+          duration: animationDuration,
+          iterations: 1,
+          timing: "ease",
+        }
+      );
+    }
+    var testCube = document.getElementById("c1");
+    Promise.all(
+      testCube
+        .getAnimations({ subtree: true })
+        .map((animation) => animation.finished)
+    ).then(() => setAnimations());
+  }
+}
+
+var paused = false;
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    if (paused == false) {
+      paused = true;
+    } else {
+      paused = false;
+      setAnimations();
+    }
+  }
+});
 
 // thisCube.onmouseover = function() {
 //     var mouseoverCube = document.getElementById(this.id);
